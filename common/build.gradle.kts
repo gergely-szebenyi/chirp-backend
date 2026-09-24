32915a1f8b4394af5bd0a67a44b583e4ce1e4384
gergely.szebenyi <gergely.szebenyi@bcsconsult.hu>
2026-09-24 13:12:00 +0200

    Creating the Gradle Modules and fix java versions

---
+++ common/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.jpa)
+    alias(libs.plugins.spring.boot)
+    alias(libs.plugins.spring.dependency.management)
+    alias(libs.plugins.kotlin.spring)
+}
+
+group = "com.prekogdevs"
+version = "unspecified"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,11 @@
+plugins {
+	id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
+}
+
 rootProject.name = "Chirp"
+
+include("app")
+include("user")
+include("chat")
+include("notification")
+include("common")
