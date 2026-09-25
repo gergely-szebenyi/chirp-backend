4804ff641078461fc13ceae969f391e698743d56
gergely.szebenyi <gergely.szebenyi@bcsconsult.hu>
2026-09-25 20:01:47 +0200

    Set up user domain models

---
+++ user/src/main/kotlin/com/prekogdevs/user/domain/model/User.kt
@@ -0,0 +1,12 @@
+package com.prekogdevs.user.domain.model
+
+import java.util.UUID
+
+typealias UserId = UUID
+
+data class User(
+    val id: UserId,
+    val username: String,
+    val email: String,
+    val hasEmailVerified: Boolean
+)
